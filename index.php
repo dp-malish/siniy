3b91d6c5cf5c21490390bd84a9fb479059fbf44d
Alexandr <admin@dp-malish.com>
2016-06-24 11:24:23 +0300

    ura2

--- index.php
+++ index.php
@@ -1,7 +1,162 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Дом
- * Date: 23.06.2016
- * Time: 19:17
- */
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Синий</title>
+<link rel="stylesheet" type="text/css" href="/css/default.css">
+<link rel="stylesheet" type="text/css" href="/css/frame_pc.css">
+<!--<link rel="stylesheet" type="text/css" href="/css/frame_m.css">-->
+<link rel="stylesheet" type="text/css" href="/css/frame.css">
+
+<link rel="stylesheet" type="text/css" href="/css/logo.css">
+
+<link rel="stylesheet" type="text/css" href="/css/color.css">
+
+
+<link rel="stylesheet" type="text/css" href="/css/temp.css">
+
+<link rel="stylesheet" type="text/css" href="/css/ind.css">
+
+</head>
+
+<body>
+<div id="bh" class="maxw"><div class="cl"></div></div>
+
+<div id="header" class="maxw rel">
+    <header>
+
+
+        <div id="logo" class="fl rel">
+
+                <div id="card">                    
+                    <figure id="logofront" class="front">
+                    
+                    
+                   
+                    
+                    
+                    <p style="color:#BD2629">передок</p>
+                    
+                    
+                    </figure>
+                    <figure id="logoback" class="back"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 238 198" style="enable-background:new 0 0 238 198;" xml:space="preserve">
+<style type="text/css">
+	.st0{fill:#432E99;}
+.wufoo-letter {
+          animation: kaboom 4.5s ease alternate infinite;
+}
+.wufoo-letter:nth-child(2) {
+
+          animation-delay: 0.15s;
+}
+.wufoo-letter:nth-child(3) {
+          animation-delay: 0.13s;
+}
+.wufoo-letter:nth-child(4) {
+          animation-delay: 0.12s;
+}
+.wufoo-letter:nth-child(5) {
+          animation-delay: 0.11s;
+}
+.wufoo-letter:nth-child(6) {
+          animation-delay: 0.14s;
+}
+.wufoo-letter:nth-child(7) {
+          animation-delay: 0.15s;
+}
+@keyframes kaboom {
+	  60% {
+            transform: scale(0.8);
+  }
+  70% {
+            transform: scale(0.9);
+  }
+    80% {
+            transform: scale(1);
+  }
+      90% {
+            transform: scale(1.1);
+  }
+  100% {
+            transform: scale(1.2);
+  }
+}
+
+</style><g>
+<path class="st0 wufoo-letter" d="M44.8,27l-3-1.5L37,24.4L33.5,24H30l-3.8,0.4l-3.9,0.7l-3.4,1.1l-2.8,1.4l-1.8,1.1L13,29.8l-1.5,1.6l-1.4,2.1
+	l-0.9,1.7l-0.7,2.3L8,39.2V41l0.5,2.1l1,2.3l1.3,2.1l1.7,1.7l1.8,1.7l2.2,1.3l2.5,1.3l2.9,1.2l9.7,3.6l2.3,1.1l2,1.2l2,2l1.1,2.4
+	l0.3,2.8l-0.3,2l-0.7,2l-1.7,2l-2.3,1.7l-2.1,0.9l-2.5,0.7l-3.5,0.7l-2.4-0.1l-2.3-0.2l-2.1-0.3l-1.5-0.5l-1.4-0.3l-1.7-0.3
+	l-2.1-0.7l-1.7-0.8l-1.8-0.8l-2.2,6l1,0.7l1.6,0.7l2,0.7l2.1,0.7l2.5,0.4l2.5,0.5l1.4,0.1l1.4,0.3l1.9,0.1l2.7-0.1l2.3-0.1l1.9-0.3
+	l2.1-0.4l2.3-0.5l2.2-0.8l1.9-0.8l1.6-0.9l1.5-0.9l1.3-1.1l1.1-1l1-1.3l0.8-1.2l0.8-1.5l0.5-1.7l0.3-1.5l0.2-1.8V66v-3l-2.6-4.9
+	l-2.3-2.3l-2.8-2.4l-4.1-2.7l-8.7-3.5L21,45.6L17.8,43L17,39.5c0,0,2-4.8,2.4-5s5.5-3.8,5.5-3.8l4.2-0.7l4.3,0.5l2.9,0.5l2.9,0.6
+	l3.9,1.5l2.5-5.9L44.8,27z"/>
+<ellipse class="st0 wufoo-letter" cx="64.1" cy="30.5" rx="5.5" ry="4.3"/>
+<rect x="60" y="41" class="st0" width="9" height="42"/>
+<polygon class="st0 wufoo-letter" points="112.7,83 112.8,57.4 112.4,55.3 111.8,53.5 110.8,51.4 109.2,49.5 107,48.3 104.6,48 102.3,47.8 
+	100,48 97.9,48.3 94.3,49.6 90.9,52.1 89.4,54.6 88.6,57.5 88.8,83 79.3,83 79.5,41.7 87.3,41.8 87.9,48.4 91.4,44.9 93.5,43.5 
+	95.7,42.6 97.2,42 99.8,41.4 102.1,41 104.5,40.8 107.7,41.1 109.9,41.6 111.3,42.1 112.5,42.7 114.1,43.4 115.3,44.3 117.3,45.9 
+	118.7,47.7 119.9,49.6 120.5,51.3 121,52.8 121.4,53.7 121.7,56 122,63.1 122,83 "/>
+<ellipse class="st0 wufoo-letter" cx="136.1" cy="30.5" rx="5.5" ry="4.3"/>
+<rect x="132" y="41" class="st0" width="9" height="42"/>
+<polygon class="st0 wufoo-letter" points="148,41 157,41 170.8,74.6 183.4,41 193,41 175.3,80.6 172.3,86.3 169.1,90.8 166.6,93.9 164,96.3 
+	161.5,98.1 158.9,99.6 155.8,101 152.1,102 150,95.6 154.2,93.8 158.3,92 161.6,89.8 163.5,86.6 165.4,83.1 165.3,80.9 147.4,41 "/>
+	</g>
+	
+	
+<polygon class="st0" points="143.8,144 139.1,142.7 135.3,141.8 130.3,142.9 126.5,145.5 123.8,149 122.5,151.8 122,157 122,161 
+	122.8,164.8 124.2,167.5 126.2,170.3 129.7,172.8 134.2,174 138,174 142,172.8 144.5,171.8 145.7,177.2 142.5,179.1 138.3,180 
+	135,180 131,180 126.8,178.9 122,176.8 118.3,173.3 115.7,168.7 114,162.8 114,157 115,150.8 117.3,145.8 120.7,141.8 126,138.2 
+	130.8,136.8 136.5,136.2 141,137 145.7,138.5 "/>
+<path class="st0" d="M184.6,146.6l-1-3.3l-1.8-3l-5-3.2l-4.8-1l-5.5,0.7l-5,0.8l-4.5,2l1.5,5.3l3.7-1.8l4.2-1.2h6.5l3,1.5l2,3.2
+	l0.4,5.2l-4.4,0.2l-4.9,1l-4.9,1.3l-5.2,2.8l-4,5.2l-0.8,7.7l2.2,4.7l3.5,3.5l4.1,1.5l5,0.3l4.2-1.1l2.8-2.2l3.2-2.9l1.2,5.3h5.7
+	v-27L184.6,146.6z M178.3,165.6c-0.2,0.5-0.1,1-0.4,1.4c-0.1,0.6-0.5,1-0.7,1.6c-1,2.3-3.1,4-5.5,4.7c-0.6,0.2-1.1,0.3-1.7,0.3
+	c-0.5,0.2-1.1,0.3-1.8,0.3c0,0-0.1,0-0.1,0c0,0,0,0-0.1,0c-0.1,0-0.2,0-0.3,0c-0.1,0-1.1,0-1.5-0.2c-1.4,0-2.7-0.6-3.6-1.6
+	c-1.3-1-2.2-2.2-2.4-3.7c-0.4-2-0.4-4,1.1-5.4c0.3-0.3,1.3-1.5,1.7-1.8c1.9-1.8,4.1-2.8,6.5-2.9c0.6-0.3,1.3-0.4,2.1-0.4
+	c0.5,0,0.9-0.1,1.4,0c1.1-0.1,2.3,0.3,3.1,0.8c1,0.6,2.1,1.8,2.3,3C178.6,163.1,178.8,164.3,178.3,165.6z"/>
+<path class="st0" d="M232.1,150.4l-1.4-3.3l-1.3-3l-2.1-2.7l-3-2.4l-3.3-1.8l-2.8-0.8l-2.8-0.4l-4.1,0.5l-3,0.8l-3.4,2l-2.2,2
+	l-2,2.6l-0.3-7L194,137v59h7.6l-0.1-22.7l3.5,3.5l2.2,1.6l3.8,1.6h4.2l3.4-0.5l4.2-1.8l2.8-2.1l2.3-2.4l2.1-3.2l1.8-4l0.8-4.2
+	l0.4-3.8v-3.4L232.1,150.4z M226.4,158.2c-0.1,2-0.7,3.9-1.5,5.7c-0.9,2.2-2.1,4.3-3.8,5.8c-1,0.9-2.3,1.6-3.7,2
+	c-2.8,0.8-5.4,0.3-7.4-1c-2.3-1.4-4.1-3.8-4.9-6.5c-0.3-1-0.5-2.1-0.6-3.2c-0.4-1.5-0.6-3.1-0.6-4.6c0-0.7,0.1-1.3,0.3-1.8
+	c-0.2-1.4,0.1-2.8,1-4c0.1-1,0.6-2,1.5-2.8c1.4-1.3,3.1-2.5,5-3.4c2.5-1.2,5.3-1.7,7.9-0.8c1.1,0.4,3.7,3.7,4.3,4.3
+	c0.7,0.7,2.8,4.4,2.6,5.8C226.5,154,226.4,158,226.4,158.2z"/>
+</svg>
+</figure>
+                </div>
+                <div class="cl"></div>
+            
+        </div>
+<script src="/js/logo.js"></script>
+        
+        
+        <div class="h_field">
+            <div class="cl"></div>
+        </div>
+        
+        
+        <div class="h_field">
+            <div class="cl"></div>
+        </div>
+        
+        <div class="cl"></div>
+    </header>
+</div>
+
+<!--<div class="maxw">
+<figure id="ani" class="logo animated fadeInDown delay-07s">
+        	<a href="#"><img src="/img/site/logo.png" alt=""></a>	
+        </figure>	
+        <h1 class="animated fadeInDown delay-07s">Welcome To Knight Studios</h1>
+        <ul class="we-create animated fadeInUp delay-1s">
+        	<li>We are a digital agency that loves crafting beautiful websites.</li>
+        </ul>
+            <a class="link animated fadeInUp delay-1s" href="#">Get Started</a>
+</div>-->
+
+
+<div class="maxw"></div>
+<div class="maxw"></div>
+<div class="maxw"></div>
+
+</body>
+</html>
