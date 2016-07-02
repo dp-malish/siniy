81b677746fb13497d8efd10ecf530d80b9917249
Alexandr <admin@dp-malish.com>
2016-07-02 23:20:51 +0300

    ura5

--- js/logo.js
+++ js/logo.js
@@ -1,6 +1,8 @@
+function mainPage(){location.href='/'}
 function logo(){
 	logofront.addEventListener('mouseenter',function(){card.classList.add("flip");})
 	logoback.addEventListener('mouseleave',function(){card.classList.remove("flip");})
-	logoback,addEventListener('click',function(){location.href='/';})
+	logofront.addEventListener('click',mainPage)
+	logoback.addEventListener('click',mainPage)
 }
 document.addEventListener("DOMContentLoaded", logo);
