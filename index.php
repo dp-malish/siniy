a26f5bdf2efbf33f7772c695057c0abf10bf5f8e
Alexandr <admin@dp-malish.com>
2016-07-06 19:42:22 +0300

    delivery start

--- index.php
+++ index.php
@@ -31,16 +31,7 @@
         <div id="logo" class="fl rel">
 
                 <div id="card">                    
-<figure id="logofront" class="front">
-                    
-                    
-                   
-                    
-                    
-                    <p style="color:#BD2629">передок надо посмотреть</p>
-                    
-                    
-</figure><figure id="logoback" class="back"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 238 198" style="enable-background:new 0 0 238 198;" xml:space="preserve"><title>Siniycap - Синийкеп</title><desc>Company logo Siniy cap with description</desc><style type="text/css">.letter{animation: kaboom 4.5s ease alternate infinite;}.letter:nth-child(2){animation-delay: 0.15s;}.letter:nth-child(3){animation-delay:0.13s;}.letter:nth-child(4){animation-delay:0.12s;}.letter:nth-child(5){animation-delay:0.11s;}.letter:nth-child(6){animation-delay:0.14s;}.letter:nth-child(7){animation-delay:0.15s;}@keyframes kaboom{60%{transform:scale(0.8);}70%{transform:scale(0.9);}80%{transform:scale(1);}90%{transform:scale(1.1);}100%{transform:scale(1.2);}}.color{fill:#432E99;}.text1,.text2,.text3{fill:#C1272D;font-family:'Verdana';font-size:24px;}.text1{animation: hideshow 8s ease infinite;}.text2{opacity:0;animation: hideshow 8s 2.5s ease infinite;}.text3{opacity:0;animation:hideshow 8s 5s ease infinite;}@keyframes hideshow{0%{opacity:1;}10%{opacity:1;}15%{opacity:0;}100%{opacity:0;}}
+<figure id="logofront" class="front"><svg version="1.1" id="svg_logo" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 226 103" style="enable-background:new 0 0 226 103;" xml:space="preserve"><title>Siniycap - logo</title><desc>Company Siniy cap - auto parts</desc><style type="text/css">.fonLogoSvg{fill:#D67C1B;}</style><polygon class="fonLogoSvg" points="64.5,27.2 64.5,46.7 26.8,46.7 25.3,46.7 23.3,47.4 21.9,48.5 20.8,51.2 20.8,53.2 20.8,55.2 22.3,57.1 24.7,58.5 26.8,59 29.3,59 64.5,59 64.5,78.2 20.8,78.2 18.3,78.2 15.3,77 11.8,75.2 9.3,73.7 7.8,72.7 6,71 4.3,68.5 2.3,66 1,63 0,56.3 0,49.3 1,42.5 2.5,39.2 3.8,37.2 5.5,35.2 7,33.7 8.5,32.2 11.1,30.1 14.5,28.2 18.3,27.2 21.3,27.2 "/><path class="fonLogoSvg" d="M225.4,38.8l-1.3-2.5l-1.7-2.3l-1.3-1.8l-1.9-1.8l-1.7-1l-1.7-1.2l-2-1l-2.5-0.9l-1.9-0.8l-2.9-0.5h-42.8v53 H183v-8.5h20.8l6.4-0.8l3.2-1.8l2.4-0.8l2.7-2l2.2-1.7l1.9-2.3l1.7-2.7l1.2-3.3l0.8-2.3v-7v-3.4L225.4,38.8z M213.1,47.5v2.3l-1,2.3 l-1.3,1.2l-1.6,1.1h-1.7h-25.3V40.7h25.3l2.2,0.1l1.6,1.1l1.5,1.7l0.3,1.3V47.5z"/><path class="fonLogoSvg" d="M164,83.5L122.7,0.7h-21.3L62.8,83.5H1V103h82.8l11.6-32.8h33.4l12.7,32.8h84.8V83.5H164z M99.7,52.7l12-33.9 l12.7,33.9H99.7z"/></svg></figure><figure id="logoback" class="back"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 238 198" style="enable-background:new 0 0 238 198;" xml:space="preserve"><title>Siniycap - Синийкеп</title><desc>Company logo Siniy cap with description</desc><style type="text/css">.letter{animation: kaboom 4.5s ease alternate infinite;}.letter:nth-child(2){animation-delay: 0.15s;}.letter:nth-child(3){animation-delay:0.13s;}.letter:nth-child(4){animation-delay:0.12s;}.letter:nth-child(5){animation-delay:0.11s;}.letter:nth-child(6){animation-delay:0.14s;}.letter:nth-child(7){animation-delay:0.15s;}@keyframes kaboom{60%{transform:scale(0.8);}70%{transform:scale(0.9);}80%{transform:scale(1);}90%{transform:scale(1.1);}100%{transform:scale(1.2);}}.color{fill:#432E99;}.text1,.text2,.text3{fill:#C1272D;font-family:'Verdana';font-size:24px;}.text1{animation: hideshow 8s ease infinite;}.text2{opacity:0;animation: hideshow 8s 2.5s ease infinite;}.text3{opacity:0;animation:hideshow 8s 5s ease infinite;}@keyframes hideshow{0%{opacity:1;}10%{opacity:1;}15%{opacity:0;}100%{opacity:0;}}
 </style><g class="color"><path class="letter" d="M48.8,23l-3-1.5L41,20.4L37.5,20H34l-3.8,0.4l-3.9,0.7l-3.4,1.1l-2.8,1.4l-1.8,1.1L17,25.8l-1.5,1.6l-1.4,2.1 l-0.9,1.7l-0.7,2.3L12,35.2V37l0.5,2.1l1,2.3l1.3,2.1l1.7,1.7l1.8,1.7l2.2,1.3l2.5,1.3l2.9,1.2l9.7,3.6l2.3,1.1l2,1.2l2,2l1.1,2.4 l0.3,2.8l-0.3,2l-0.7,2l-1.7,2l-2.3,1.7l-2.1,0.9l-2.5,0.7l-3.5,0.7l-2.4-0.1l-2.3-0.2l-2.1-0.3l-1.5-0.5l-1.4-0.3l-1.8-0.3 l-2.1-0.7l-1.7-0.8l-1.8-0.8l-2.3,6l1,0.7l1.6,0.7l2,0.7l2.1,0.7l2.5,0.4l2.5,0.5l1.4,0.1l1.4,0.3l1.9,0.1l2.7-0.1l2.3-0.1l1.9-0.3 l2.1-0.4l2.3-0.5l2.2-0.8l1.9-0.8l1.6-0.9l1.5-0.9l1.3-1.1l1.1-1l1-1.3l0.8-1.2l0.8-1.5l0.5-1.7l0.3-1.5l0.2-1.8V62v-3l-2.6-4.9 l-2.3-2.3l-2.8-2.4l-4.1-2.7l-8.7-3.5L25,41.6L21.8,39L21,35.5c0,0,2-4.8,2.4-5s5.5-3.8,5.5-3.8l4.2-0.7l4.3,0.5l2.9,0.5l2.9,0.6 l3.9,1.5l2.5-5.9L48.8,23z"/><ellipse class="letter" cx="68.1" cy="26.5" rx="5.5" ry="4.3"/><rect x="64" y="37" width="9" height="42"/><polygon class="letter" points="116.7,79 116.8,53.4 116.4,51.3 115.8,49.5 114.8,47.4 113.2,45.5 111,44.3 108.6,44 106.3,43.8 104,44 101.9,44.3 98.3,45.6 94.9,48.1 93.4,50.6 92.6,53.5 92.8,79 83.3,79 83.5,37.7 91.3,37.8 91.9,44.4 95.4,40.9 97.5,39.5 99.7,38.6 101.2,38 103.8,37.4 106.1,37 108.5,36.8 111.7,37.1 113.9,37.6 115.3,38.1 116.5,38.7 118.1,39.4 119.3,40.3 121.3,41.9 122.7,43.7 123.9,45.6 124.5,47.3 125,48.8 125.4,49.7 125.7,52 126,59.1 126,79 "/><ellipse class="letter" cx="140.1" cy="26.5" rx="5.5" ry="4.3"/><rect x="136" y="37" width="9" height="42"/><polygon class="letter" points="152,37 161,37 174.8,70.6 187.4,37 197,37 179.3,76.6 176.3,82.3 173.1,86.8 170.6,89.9 168,92.3 165.5,94.1 162.9,95.6 159.8,97 156.1,98 154,91.6 158.2,89.8 162.3,88 165.6,85.8 167.5,82.6 169.4,79.1 169.3,76.9 151.4,37 "/></g><g class="color"><polygon points="144.8,110 140.1,108.7 136.3,107.8 131.3,108.9 127.5,111.5 124.8,115 123.5,117.8 123,123 123,127 123.8,130.8 125.2,133.5 127.2,136.3 130.7,138.8 135.2,140 139,140 143,138.8 145.5,137.8 146.7,143.2 143.5,145.1 139.3,146 136,146 132,146 127.8,144.9 123,142.8 119.3,139.3 116.7,134.7 115,128.8 115,123 116,116.8 118.3,111.8 121.7,107.8 127,104.2 131.8,102.8 137.5,102.2 142,103 146.7,104.5 "/><path d="M185.6,112.6l-1-3.3l-1.8-3l-5-3.2l-4.8-1l-5.5,0.7l-5,0.8l-4.5,2l1.5,5.3l3.7-1.8l4.2-1.2h6.5l3,1.5l2,3.2 l0.4,5.2l-4.4,0.2l-4.9,1l-4.9,1.3l-5.2,2.8l-4,5.2l-0.8,7.7l2.2,4.7l3.5,3.5l4.1,1.5l5,0.3l4.2-1.1l2.8-2.2l3.2-2.9l1.2,5.3h5.7 v-27L185.6,112.6z M179.3,131.6c-0.2,0.5-0.1,1-0.4,1.4c-0.1,0.6-0.5,1-0.7,1.6c-1,2.3-3.1,4-5.5,4.7c-0.6,0.2-1.1,0.3-1.7,0.3 c-0.5,0.2-1.1,0.3-1.8,0.3c0,0-0.1,0-0.1,0c0,0,0,0-0.1,0c-0.1,0-0.2,0-0.3,0c-0.1,0-1.1,0-1.5-0.2c-1.4,0-2.7-0.6-3.6-1.6 c-1.3-1-2.2-2.2-2.4-3.7c-0.4-2-0.4-4,1.1-5.4c0.3-0.3,1.3-1.5,1.7-1.8c1.9-1.8,4.1-2.8,6.5-2.9c0.6-0.3,1.3-0.4,2.1-0.4 c0.5,0,0.9-0.1,1.4,0c1.1-0.1,2.3,0.3,3.1,0.8c1,0.6,2.1,1.8,2.3,3C179.6,129.1,179.8,130.3,179.3,131.6z"/><path d="M233.1,116.4l-1.4-3.3l-1.3-3l-2.1-2.7l-3-2.4l-3.3-1.8l-2.8-0.8l-2.8-0.4l-4.1,0.5l-3,0.8l-3.4,2l-2.2,2 l-2,2.6l-0.3-7L195,103v59h7.6l-0.1-22.7l3.5,3.5l2.2,1.6l3.8,1.6h4.2l3.4-0.5l4.2-1.8l2.8-2.1l2.3-2.4l2.1-3.2l1.8-4l0.8-4.2 l0.4-3.8v-3.4L233.1,116.4z M227.4,124.2c-0.1,2-0.7,3.9-1.5,5.7c-0.9,2.2-2.1,4.3-3.8,5.8c-1,0.9-2.3,1.6-3.7,2 c-2.8,0.8-5.4,0.3-7.4-1c-2.3-1.4-4.1-3.8-4.9-6.5c-0.3-1-0.5-2.1-0.6-3.2c-0.4-1.5-0.6-3.1-0.6-4.6c0-0.7,0.1-1.3,0.3-1.8 c-0.2-1.4,0.1-2.8,1-4c0.1-1,0.6-2,1.5-2.8c1.4-1.3,3.1-2.5,5-3.4c2.5-1.2,5.3-1.7,7.9-0.8c0.6,0.2,2.2,0.9,3.1,1.9 c0.9,1,1.3,1.7,1.6,2c0.3,0.4,0.9,1.6,1.5,2.8c0.6,1.2,0.8,2.7,0.7,3.4C227.5,120,227.4,124,227.4,124.2z"/></g><text transform="matrix(1 0 0 1 35 180)" class="text1">Автозапчасти</text><text transform="matrix(1 0 0 1 60 180)" class="text2">в наличии</text><text transform="matrix(1 0 0 1 47 180)" class="text3">и под заказ</text></svg></figure>
                 </div><div class="cl"></div>
             
@@ -111,13 +102,23 @@
             <div class="cl"></div>
         </div><!--Центр кол-->
         <div id="c_col" class="rel">
+            <div class="fon_c rel five_p">
             <div id="main_slider" class="br">
-                <img alt src="/img/site/slider/magazine.png" alt="">
-                <img alt src="/img/site/slider/zapchasti.png" alt="">
-                <img alt src="/img/site/slider/logistic.png" alt="">
+                <img src="/img/site/slider/magazine.png" alt="">
+                <img src="/img/site/slider/zapchasti.png" alt="">
+                <img src="/img/site/slider/logistic.png" alt="">
                 <div></div>
             </div>
-            <main><?= ''; ?></main>
+            <main>
+                    <h2>Магазин автозапчастей - Синий кеп</h2>
+                    <span class="h_ext">Дорогие посетители!</span>
+
+                    <p>Мы рады приветствовать Вас в магазине «Синий кеп» и будем рады представить широкий ассортимент товаров, разумные цены и своевременную доставку приобретенных у нас автозапчастей в любую точку Украины.</p>
+                    <p>Наша цель: работать с качественными производителями автозапчастей и поставщиками автозапчастей, чтоб всегда соответствовать всем требованиям современного рынка товаров и услуг.</p>
+                    <p>Мы ценим каждого клиента и готова предложить выгодные условия для покупки интересующих Вас комплектующих или запчастей. Обширный ассортимент представленных у нас товаров удовлетворит потребности любого покупателя.</p>
+                    <p>Благодарим Вас за проявленный к нам интерес!</p>
+                </main>
+            </div>
             <div class="cl"></div>
         </div><!--Конец Центр кола-->
         <div class="cl"></div>
@@ -125,21 +126,63 @@
     <div class="cl"></div>
 </div>
 
-<div id="delivery" class="maxw rel">
-    <div class="fon_c">
-        <h2>Доставка автозапчастей</h2>
+<div class="maxw space"></div>
+
+<div id="about" class="maxw rel">
+    <div class="fon_ce "><img class="fl five" src="/img/site/png.php?img=about" alt="о нас"><img class="fr five" src="/img/site/png.php?img=about_r" alt="о нас">
+        <h2>О нас</h2>
+        <p>Мы еще не первые в своей отрасли, а потому нам приходится стараться больше других, чтобы доказать, что мы достойны быть первыми. Мы работаем без праздников и выходных. Все ради того, чтобы клиент был счастлив.</p><p>Около 80% наших сотрудников работают в магазине более 10 лет, а потому имеют опыт ошибок и открытий, который позволяет нам своевременно реагировать на изменения рынка товаров и услуг.</p>
+        <div class="cl"></div>
     </div>
+    <div class="fon_ce ff fl">
+        <div class="item al">
+        <h3>Преимущества сотрудничества с нами</h3>
+            <p><span class="check"></span>Внутренний контроль качества: мы не закупаем второсортную продукцию.</p>
+            <p><span class="check"></span>Гарантийные сроки и развитое постпродажное обслуживание.</p>
+            <p><span class="check"></span>Оптовые закупки у поставщиков, а потому – низкие цены в розницу.</p>
+            <p><span class="check"></span>Постоянный мониторинг рынка, поиск лучших товаров.</p>
+            <p><span class="check"></span>Умение выслушать и понять покупателя.</p>
+            <p><span class="check"></span>Отсутствие агрессивной маркетинговой политики: мы не будем навязывать вам обязательную покупку.</p>
+            <p><span class="check"></span>Удобное географическое расположение.</p>
+            <p><span class="check"></span>Регулярные сезонные акции с очень серьезными скидками.</p>
+            <p><span class="check"></span>Советуем лучшее, а не продаем самое завалявшееся и дорогое.</p>
+            <p><span class="check"></span>Это нормально, когда товар не совсем подходит. Мы вернем вам деньги без каких-либо проблем, долгих разбирательств и прочей бюрократии.</p>
+        </div>
+        <div class="cl"></div>
+    </div>
+    <div class="fon_ce ff fr">
+        <div class="item al">
+        <h3>Достоинства наших поставщиков</h3>
+            <p><span class="check"></span>Постоянное обновление технологии, выпуск действительно хорошей продукции.</p>
+            <p><span class="check"></span>Льготное налогообложение (минимальные тарифы на электричество и т.д.), благодаря чему удается снизить стоимость продукции.</p>
+            <p><span class="check"></span>Сильная инженерная команда.</p>
+            <p><span class="check"></span>Отсутствие наценок по регионам.</p>
+            <p><span class="check"></span>Продажа продукции только напрямую, минуя цепь посредников.</p>
+            <p><span class="check"></span>Отсутствие нецелевых расходов, реструктуризация производства помогают снизить стоимость продукции для конечного потребителя.</p>
+            <p><span class="check"></span>Повышение качества продукции по сравнению с позапрошлым годом.</p>
+            <p><span class="check"></span>Наличие в штате специалистов «старой школы», которые отвечают за передачу важного и редкого опыта молодым коллегам.</p>
+            <p><span class="check"></span>Всегда на связи и готовы проконсультировать по любому вопросу.</p>
+        </div>
+        <div class="cl"></div>
+    </div>
+    <div class="cl"></div>
 </div>
 
-<div class="maxw rel">
-    <div class="l_col fl">
+<div class="maxw space"></div>
 
+<div id="delivery" class="maxw rel">
+    <img id="plane" class="z2" src="/img/site/png.php?img=plane_">
+    <div class="l_col fl rel">
+        <div class="fon_ce ac z3 rel">
+            <h2>Доставка автозапчастей</h2>
+            <img src="/img/site/png.php?img=delivery">
+        </div>
     </div>
-    <div class="m_col rel fl">
-        jkljlkjlkjkljlk567757667556 5657656756   67567567567 6 5675 67 567 567576 5765 76 5jlkjkljlkjlk jkljlkjlk<br>jkljlkjlk<br>jkljlkjlk<br>jkljlkjlk<br>jkljlkjlk<br>jkljlkjlk<br>
-        jkljlkjlk<br>jkljlkjlk<br>jkljlkjlk<br>jkljlkjlk<br>jkljlkjlk<br>
-        jkljlkjlk<br>jkljlkjlk<br>jkljlkjlk<br>jkljlkjlk<br>jkljlkjlk<br>jkljlkjlk<br>
-        jkljlkjlk<br>jkljlkjlk<br>jkljlkjlk<br>jkljlkjlk<br>jkljlkjlk<br>jkljlkjlk<br>
+    <div class="m_col rel mt">
+        <div class="fon_c">
+
+            fdg uyi yui uyi u uyiyui uu <br> jhj<br>k jkl jkl jkl<br> jkl kj jk kj jkl jk jk k jjk kj kj kj jk lk jjk ljkk jjkl jkl jkl kjl jkl jkl kj kj kj ljk kj kj k j k kk kj kl klj kjjk jkjkjkj kjk j jkjkj kjkjjkjk
+        </div>
     </div>
     <div class="cl"></div>
 </div>
@@ -147,22 +190,16 @@
 
 <div class="maxw rel">
     <div class="l_col fl">
-        <a id="top"><h1>Заголовок</h1></a>
+
         <p>Многостраничный текст (иначе результат не будет заметен)</p>
     </div>
     <div class="m_col rel fl">
-        jkljlkjlk<br>jkljlkjlk<br>
-        jkljlkjlk<br>
-        jkljlkjlk<br>jkljlkjlk<br>jkljlkjlk<br>jkljlkjlk<br>jkljlkjlk<br>jkljlkjlk<br>
-        jkljlkjlk<br>jkljlkjlk<br>jkljlkjlk<br>jkljlkjlk<br>jkljlkjlk<br>
-        jkljlkjlk<br>jkljlkjlk<br>jkljlkjlk<br>jkljlkjlk<br>jkljlkjlk<br>jkljlkjlk<br>
-        jkljlkjlk<br>jkljlkjlk<br>jkljlkjlk<br>jkljlkjlk<br>jkljlkjlk<br>jkljlkjlk<br>
-        jkljlkjlk<br>
+        rgregreg
     </div>
     <div class="cl"></div>
 </div>
 
-<div id="about" class="maxw rel"></div>
+<div class="maxw rel"></div>
 
 
 <div id="bf" class="maxw cl"></div>
